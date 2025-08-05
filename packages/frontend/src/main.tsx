6a365cff7495002cf8763efa2dbb6048dda3a873
Nicola Abis <nicobees@gmail.com>
2025-08-05 20:41:11 +0200

    feat: update eslint config with full fledge plugins

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.tsx';
 
 const router = createBrowserRouter([
   {
-    path: '/',
     element: <App />,
+    path: '/',
   },
 ]);
 
--- packages/frontend/src/validation/schema.ts
+++ packages/frontend/src/validation/schema.ts
@@ -10,12 +10,12 @@ export type PatientInfo = z.infer<typeof PatientInfoSchema>;
 
 // Zod schema for LeftRightSpecs
 const LensSpecsSchema = z.object({
+  add: z.string().regex(/^[+-]?\d{1,2}\.\d{2}$/, 'Invalid ADD format'),
+  ax: z.string().regex(/^\d{3}$/, 'Invalid AX format'),
   bc: z.string().regex(/^\d{1,2}\.\d{2}$/, 'Invalid BC format'),
+  cyl: z.string().regex(/^[+-]?\d{1,2}\.\d{2}$/, 'Invalid CYL format'),
   dia: z.string().regex(/^\d{1,2}\.\d{2}$/, 'Invalid DIA format'),
   pwr: z.string().regex(/^[+-]?\d{1,2}\.\d{2}$/, 'Invalid PWR format'),
-  cyl: z.string().regex(/^[+-]?\d{1,2}\.\d{2}$/, 'Invalid CYL format'),
-  ax: z.string().regex(/^\d{3}$/, 'Invalid AX format'),
-  add: z.string().regex(/^[+-]?\d{1,2}\.\d{2}$/, 'Invalid ADD format'),
   sag: z.string().regex(/^\d{1,2}\.\d{2}$/, 'Invalid SAG format'),
 });
 
@@ -31,13 +31,13 @@ export type LensesSpecs = z.infer<typeof LensesSpecsSchema>;
 
 // Zod schema for the main LabelData
 export const LabelDataSchema = z.object({
-  patient_info: PatientInfoSchema,
-  description: z.string().min(0).max(24),
   batch: z
     .string()
     .regex(/^\d{2}-\d{4}$/, 'Invalid batch format (e.g., 25-0001)'),
+  description: z.string().min(0).max(24),
   due_date: z.string().regex(/^\d{2}\/\d{4}$/, 'Invalid date format (MM/YYYY)'),
   lens_specs: LensesSpecsSchema,
+  patient_info: PatientInfoSchema,
 });
 
 export type LabelData = z.infer<typeof LabelDataSchema>;
